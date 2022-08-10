20a1745dbc9fe0bf461b203e50b9be6164292747
KingPereira <g1l14rd3@gmail.com>
2022-08-10 08:31:28 -0300

    creche

--- creche/routes/web.php
+++ creche/routes/web.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Http\Controllers\PostoController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,5 +16,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
-Route::get('/inicio', [PostoController::class, 'index']);
